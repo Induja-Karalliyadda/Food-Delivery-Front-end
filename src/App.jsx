5491aa44d11ae6a25faf75fffd82926b09da271f
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-06-05 14:04:55 +0530

    started nav bar orderlist

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return (
-    <div className="app">App
+    <div className="app">
        <Navbar/>
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
+import { assets } from '../../assets/assets'
 import './Navbar.css'
 
 function Navbar() {
   return (
     <div className='navbar'>
-        Navbar</div>
+        <img src={assets.logo} alt="" className='logo'/>
+        <ul className="navbar-menu">
+            <li>home</li>
+            <li>menu</li>
+            <li>mobile-app</li>
+            <li>contact us</li>
+        </ul>
+        <div className="navbar-right"></div>
+        </div>
   )
 }
 
