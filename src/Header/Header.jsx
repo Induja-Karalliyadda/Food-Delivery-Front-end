a557aeb8bd5077ca762680feb13f700af16fc9ac
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-06-07 21:10:20 +0530

    factor: update Header component

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,7 +3,10 @@ import './Header.css'
 function Header() {
   return (
     <div className='header'>
-        
+        <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad adipisci aut minus et ab sequi, laboriosam culpa quos ducimus provident reiciendis. Nesciunt magni repellendus, tenetur beatae labore sunt soluta sed.</p>
+        <button>View Menu</button></div>
     </div>
   )
 }
