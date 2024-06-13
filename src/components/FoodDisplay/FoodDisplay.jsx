3291d347f697962f864b731a11c8af4e1a47c188
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-06-13 20:47:07 +0530

    feat: create FoodDisplay component

---
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,14 @@
+import { useContext } from 'react'
+import './FoodDisplay.css'
+import { StoreContext } from '../../context/StoreContext'
+
+function FoodDisplay() {
+    const{food_list} =useContext(StoreContext)
+  return (
+    <div className='food-display' id='food-display'>
+       <h2>Top dishes near you</h2>
+        </div>
+  )
+}
+
+export default FoodDisplay
