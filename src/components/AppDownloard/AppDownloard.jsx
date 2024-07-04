90396f839581e0c645815d0ba4d44a86e3280beb
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-07-04 13:31:28 +0530

    Create and style new element in App component

--- src/components/AppDownloard/AppDownloard.jsx
+++ src/components/AppDownloard/AppDownloard.jsx
@@ -1,9 +1,14 @@
+import { assets } from '../../assets/assets'
 import './AppDownloard.css'
 
 const AppDownloard = () => {
   return (
     <div className='app-download' id='app-download'>
       <p>For Better Experiance Download <br />Tomato App</p>
+    <div className='app-download-platfoems'>
+    <img src={assets.play_store} alt="" />
+    <img src={assets.app_store} alt="" />
+    </div>
     </div>
   )
 }
