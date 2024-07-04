4cc051e13a39ddc59d268d63d7648a06e75b3ad6
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-07-04 12:59:35 +0530

    create AppDownload component

---
+++ src/components/AppDownloard/AppDownloard.jsx
@@ -0,0 +1,11 @@
+import './AppDownloard.css'
+
+const AppDownloard = () => {
+  return (
+    <div className='app-download' id='app-download'>
+      <p>For Better Experiance Download <br />Tomato App</p>
+    </div>
+  )
+}
+
+export default AppDownloard
