0e43e8f3c8c8f3d392c3d8bac3ed35223f190053
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-07-09 20:53:39 +0530

    add navigation part

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,10 +9,10 @@ function Navbar() {
     <div className='navbar'>
         <img src={assets.logo} alt="" className='logo'/>
         <ul className="navbar-menu">
-            <Link onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
-            <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-            <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
-            <a href='' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+            <Link onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+            <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
+            <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
+            <a href='' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
         </ul>
         <div className="navbar-right"></div>
         <img src={assets.search_icon} alt="" />
